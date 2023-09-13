f75c8980829811f90d783ffe3b647732da8e6b63
Edgar José García Landaeta <edgar.landaeta@tideworks.com>
2023-09-12 19:18:20 -0500

    cambio

--- src/main/java/co/edu/eafit/protobootapp/Calculadora.java
+++ src/main/java/co/edu/eafit/protobootapp/Calculadora.java
@@ -6,7 +6,6 @@ public class Calculadora {
     public Double cuadrado(Double numero) {
         return numero * numero;
     }
-    
     public Double cubo(Double numero) {
         return numero * numero * numero;
     }
